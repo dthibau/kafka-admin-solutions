45b032c82548bf4a5c446b6229314b541be0b0af
David THIBAU <david.thibau@gmail.com>
2023-06-19 11:41:22 +0200

    Setup Cluster

---
+++ start-cluster.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+export KAFKA_DIST=/home/dthibau/Formations/Kafka/MyWork/kafka_2.13-3.3.1
+export KAFKA_CLUSTER=/home/dthibau/Formations/Kafka/github/solutions/kafka-cluster
+export KAFKA_LOGS=/home/dthibau/Formations/Kafka/MyWork/kafka-logs
+
+
+$KAFKA_DIST/bin/kafka-server-start.sh -daemon $KAFKA_CLUSTER/broker-1/server.properties 
+$KAFKA_DIST/bin/kafka-server-start.sh -daemon $KAFKA_CLUSTER/broker-2/server.properties 
+$KAFKA_DIST/bin/kafka-server-start.sh -daemon $KAFKA_CLUSTER/broker-3/server.properties 

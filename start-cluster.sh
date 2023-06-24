3e939073d98fc3e3adeafa5e4f56f21f557212d5
David THIBAU <david.thibau@gmail.com>
2023-06-24 23:28:45 +0200

    SASL_SSL between brokers

--- start-cluster.sh
+++ start-cluster.sh
@@ -4,7 +4,7 @@ export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64/
 export KAFKA_DIST=/home/dthibau/Formations/Kafka/MyWork/kafka_2.13-3.5.0
 export KAFKA_CLUSTER=/home/dthibau/Formations/Kafka/github/solutions/kafka-cluster
 export KAFKA_LOGS=/home/dthibau/Formations/Kafka/MyWork/kafka-logs
-export KAFKA_OPTS="-Djavax.net.debug=ssl:handshake:verbose"
+export KAFKA_OPTS="-Djavax.net.debug=ssl:handshake:verbose -Djava.security.auth.login.config=/home/dthibau/Formations/Kafka/github/solutions/kafka_server_jaas.conf"
 
 
 $KAFKA_DIST/bin/kafka-server-start.sh -daemon $KAFKA_CLUSTER/broker-1/server.properties 

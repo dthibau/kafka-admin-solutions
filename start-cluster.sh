436a6a8dc821fed9086e9612bf6db9dd0266b3f8
David THIBAU <david.thibau@gmail.com>
2023-06-24 16:15:33 +0200

    JAVA_HOME

--- start-cluster.sh
+++ start-cluster.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64/
 export KAFKA_DIST=/home/dthibau/Formations/Kafka/MyWork/kafka_2.13-3.3.1
 export KAFKA_CLUSTER=/home/dthibau/Formations/Kafka/github/solutions/kafka-cluster
 export KAFKA_LOGS=/home/dthibau/Formations/Kafka/MyWork/kafka-logs

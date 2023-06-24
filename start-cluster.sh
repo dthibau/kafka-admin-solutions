f6fc730b4404d356f477c18f29e0b4abd43e27a0
David THIBAU <david.thibau@gmail.com>
2023-06-24 17:55:22 +0200

    Kafka connecte alimente ELK

--- start-cluster.sh
+++ start-cluster.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64/
-export KAFKA_DIST=/home/dthibau/Formations/Kafka/MyWork/kafka_2.13-3.3.1
+export KAFKA_DIST=/home/dthibau/Formations/Kafka/MyWork/kafka_2.13-3.5.0
 export KAFKA_CLUSTER=/home/dthibau/Formations/Kafka/github/solutions/kafka-cluster
 export KAFKA_LOGS=/home/dthibau/Formations/Kafka/MyWork/kafka-logs
 

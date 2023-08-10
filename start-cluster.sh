281911f2efbeba755512e5820dfd8944ab2ac9f9
David THIBAU <david.thibau@gmail.com>
2023-08-10 15:44:36 +0200

    Downgrade to 3.21 for ACLs

--- start-cluster.sh
+++ start-cluster.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64/
-export KAFKA_DIST=/home/dthibau/Formations/Kafka/MyWork/kafka_2.13-3.5.0
+export KAFKA_DIST=/home/dthibau/Formations/Kafka/MyWork/kafka_2.13-3.2.1
 export KAFKA_CLUSTER=/home/dthibau/Formations/Kafka/github/solutions/kafka-cluster
 export KAFKA_LOGS=/home/dthibau/Formations/Kafka/MyWork/kafka-logs
 export KAFKA_OPTS="-Djavax.net.debug=ssl:handshake:verbose -Djava.security.auth.login.config=/home/dthibau/Formations/Kafka/github/solutions/kafka_server_jaas.conf"
